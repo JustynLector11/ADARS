215fb0fd7a6b82d975ac8fe7acadd6423edeae1c
JustynLector11 <jlectorbustamante@gmail.com>
2025-03-03 10:20:32 -0500

    Update breadcrumb.js

--- breadcrumb.js
+++ breadcrumb.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); // Capitaliza solo la primera letra
 
-    let breadcrumbHTML = `<a href="/index.html">Inicio</a>`; // Enlace al inicio
+    let breadcrumbHTML = `<a href="/ADARS/index.html">Inicio</a>`; // Enlace al inicio
 
     // Si estamos en un mes (ejemplo: eventos/2025/marzo/ o eventos/2025/marzo/index.html)
     if ((pathArray.length === 3 || (pathArray.length === 4 && pathArray[3] === "index.html")) 
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .replace(/-/g, " ")
             .replace(/\b\w/g, c => c.toUpperCase());
 
-        breadcrumbHTML += ` » <a href="/eventos/2025/${pathArray[2]}/">${mes}</a> » <span>${evento}</span>`;
+        breadcrumbHTML += ` » <a href="/ADARS/eventos/2025/${pathArray[2]}/">${mes}</a> » <span>${evento}</span>`;
     }
 
     breadcrumb.innerHTML = breadcrumbHTML;
